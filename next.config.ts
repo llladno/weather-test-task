b1a59d66ad3187dc22075dae47d329b500fda865
llladno <man30968@gmail.com>
2026-08-12 20:36:48 +0500

    chore: set up project tooling, dependencies, and design tokens

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "openweathermap.org", pathname: "/img/wn/**" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+    ],
+  },
+};
 
 export default nextConfig;
